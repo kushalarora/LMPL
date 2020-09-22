125cca1e3ee167571081c31222e2ddb6555ebddc
Kushal Arora <kushal18@gmail.com>
2020-09-22 03:13:00 -0400

    updated the version  to 0.1.1

--- lmpl/version.py
+++ lmpl/version.py
@@ -3,7 +3,7 @@ import os
 
 _MAJOR = "0"
 _MINOR = "1"
-_PATCH = "0"
+_PATCH = "1"
 # This is mainly for nightly builds which have the suffix ".dev$DATE". See
 # https://semver.org/#is-v123-a-semantic-version for the semantics.
 _SUFFIX = os.environ.get("LMPL_VERSION_SUFFIX", "")
