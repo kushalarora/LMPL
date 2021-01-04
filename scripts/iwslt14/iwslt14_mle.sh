922b69db58407780effe54a9943731ea4f752ddd
Kushal Arora <kushal18@gmail.com>
2021-01-04 16:22:38 -0500

    moved the runs to scratch.

--- scripts/iwslt14/iwslt14_mle.sh
+++ scripts/iwslt14/iwslt14_mle.sh
@@ -2,6 +2,6 @@
 
 set -eux
 export run_id=$(date '+%Y_%m_%d_%H_%M')
-OUT_DIR=results/iwslt/mle/$run_id/
+OUT_DIR=~/scratch/lmpl/results/iwslt/mle/$run_id/
 
-allennlp train training_configs/iwslt/iwslt14_de_en.jsonnet -s ${OUT_DIR}/mle/ --include-package lmpl
+sbatch -J iwslt_transformer -t 11:00:00 ./launcher_basic.sh allennlp train training_configs/iwslt/iwslt14_de_en.jsonnet -s ${OUT_DIR}/mle/ --include-package lmpl
